e155f86bd283b63c4419768052e18ac500f47b57
Hugo Pobil <hpascual@mrhouston.net>
2022-03-11 22:57:24 +0100

    primeras pruebas tweepy

---
+++ data/tweepy/get_tweets.py
@@ -0,0 +1,12 @@
+import credentials
+import tweepy
+
+# Authenticate the keys
+auth = tweepy.OAuthHandler(credentials.API_KEY, credentials.API_KEY_SECRET)
+auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+
+# API
+api = tweepy.API(auth)
+
+# Download tweets by hashtags
+
