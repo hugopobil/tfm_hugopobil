69c80e086dc1b52e8f5389fe3d1215ff502e7516
Hugo Pobil <hpascual@mrhouston.net>
2022-03-12 19:03:14 +0100

    update TFM

---
+++ scratches/bitcoin_price_analysis_kaggle/sentiment_analyser_sklearn.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as num
+import matplotlib.pyplot as plot
+import seaborn as sb
+from sklearn.feature_extraction.text import CountVectorizer
+count=CountVectorizer()
+
+data = pd.read_csv("tweets_scores.csv")
+
+
+
+
+
