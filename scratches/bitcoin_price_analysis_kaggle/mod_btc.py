c476539b3159357591a34f40b5d79f1a95b085ee
Hugo Pobil <hpascual@mrhouston.net>
2022-03-15 20:11:33 +0100

    update TFM

--- scratches/bitcoin_price_analysis_kaggle/mod_btc.py
+++ scratches/bitcoin_price_analysis_kaggle/mod_btc.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 
 def crosscorr(detax, detay, lag=0, method=str):
@@ -30,3 +31,22 @@ def plot_two_axes(tweets, bitcoin):
     plt.show()
 
 
+def normalization_plot(tweets, price):
+    min_max_scaler = preprocessing.StandardScaler()
+    score_scaled = min_max_scaler.fit_transform(tweets['score'].values.reshape(-1, 1))
+    tweets['normalized_score'] = score_scaled
+    # crypto_used_grouped_scaled = min_max_scaler.fit_transform(crypto_usd_grouped.values.reshape(-1,1))
+    crypto_used_grouped_scaled = price / max(price.max(), abs(price.min()))
+    # crypto_usd_grouped['normalized_price'] = crypto_used_grouped_scaled
+
+    fig, ax1 = plt.subplots(figsize=(20, 10))
+    ax1.set_title("Normalized Bitcoin currency evolution compared to normalized twitter sentiment", fontsize=18)
+    ax1.tick_params(labelsize=14)
+
+    ax2 = ax1.twinx()
+    ax1.plot_date(tweets.index, tweets['normalized_score'], 'g-')
+    ax2.plot_date(price.index, crypto_used_grouped_scaled, 'b-')
+
+    ax1.set_ylabel("Sentiment", color='g', fontsize=16)
+    ax2.set_ylabel("Bitcoin normalized", color='b', fontsize=16)
+    return plt.show()
